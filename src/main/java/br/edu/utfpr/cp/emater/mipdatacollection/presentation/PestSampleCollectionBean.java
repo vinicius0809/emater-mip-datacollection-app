b05602dc3af029450a4130057b2d30482a351be0
gabriel <gabriel@10.20.12.22>
2018-04-13 21:14:07 -0300

    - Created pages for date, crop & pest selection; - Created page for data insertion; - Refactored names for managed bean & main page.

--- src/main/java/br/edu/utfpr/cp/emater/mipdatacollection/presentation/PestSampleCollectionBean.java
+++ src/main/java/br/edu/utfpr/cp/emater/mipdatacollection/presentation/PestSampleCollectionBean.java
@@ -1,22 +1,33 @@
 package br.edu.utfpr.cp.emater.mipdatacollection.presentation;
 
+import br.edu.utfpr.cp.emater.mipdatacollection.model.Crop;
+import br.edu.utfpr.cp.emater.mipdatacollection.model.Farmer;
+import br.edu.utfpr.cp.emater.mipdatacollection.model.Harvest;
 import br.edu.utfpr.cp.emater.mipdatacollection.model.Pest;
 import br.edu.utfpr.cp.emater.mipdatacollection.model.PestType;
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
+import org.primefaces.event.SelectEvent;
 
 @ManagedBean
-public class DataCollectionBean implements Serializable {
+@SessionScoped
+public class PestSampleCollectionBean implements Serializable {
 
     private List<Pest> pestList;
     private Pest pestSelected;
     private PestSample sample;
+    private Date sampleCollectionDate;
+    private List<Crop> cropList;
+    private Crop cropSelected;
 
     @PostConstruct
     public void initPestList() {
@@ -25,6 +36,22 @@ public class DataCollectionBean implements Serializable {
                 new Pest("Falsa Medideira", "Chrysodeixis ssp.", "http://www.cnpso.embrapa.br/helicoverpa/folha_large/_9148710221.jpg", PestType.LESS_THAN_15CM),
                 new Pest("Lagarta das Vagens", "Spodoptera ssp.", "http://www.cnpso.embrapa.br/helicoverpa/vagens_large/_4331614441.jpg", PestType.LESS_THAN_15CM)
         ).collect(Collectors.toList());
+
+        cropList = Stream.of(
+                new Crop("Potência RR", new Farmer("José TESTE da Silva", "http://"), new Harvest("Safra 2017/2018")),
+                new Crop("Potência XX", new Farmer("Antônio TESTE da Silva", "http://"), new Harvest("Safra 2017/2018"))
+        ).collect(Collectors.toList());
+    }
+
+    public String onSampleCollectionCrop() {
+        
+        return "pm:pestSelectionInternalPage?transition=slide";
+    }
+    
+    public String onSampleCollectionDate() {
+        System.out.println(this.sampleCollectionDate);
+
+        return "pm:sampleCollectionCropPage?transition=slide";
     }
 
     public String selectPest() {
@@ -43,12 +70,28 @@ public class DataCollectionBean implements Serializable {
 
         return "pm:pestCollectionInternalPage?transition=slide";
     }
+    
+    public String selectCrop() {
+
+        String cropName = FacesContext.getCurrentInstance().
+                getExternalContext().getRequestParameterMap().get("cropName");
+
+        this.cropSelected = this.
+                getCropList().
+                stream().
+                filter(
+                        crop -> crop.getName().equalsIgnoreCase(cropName)).
+                findFirst().
+                orElseThrow(() -> new RuntimeException("Cultivar não encontrada!"));
+
+        return "pm:pestSelectionInternalPage?transition=slide";
+    }
 
     public String save() {
-        
+
         this.sample.setPest(pestSelected);
         System.out.println(this.sample);
-        
+
         FacesContext.getCurrentInstance().addMessage(null,
                 new FacesMessage(FacesMessage.SEVERITY_INFO,
                         "Coleta realizada com sucesso!", "Sucesso"));
@@ -79,4 +122,30 @@ public class DataCollectionBean implements Serializable {
         this.sample = sample;
     }
 
+    public Date getSampleCollectionDate() {
+        return sampleCollectionDate;
+    }
+
+    public void setSampleCollectionDate(Date sampleCollectionDate) {
+        this.sampleCollectionDate = sampleCollectionDate;
+    }
+
+    public List<Crop> getCropList() {
+        return cropList;
+    }
+
+    public void setCropList(List<Crop> cropList) {
+        this.cropList = cropList;
+    }
+
+    public Crop getCropSelected() {
+        return cropSelected;
+    }
+
+    public void setCropSelected(Crop cropSelected) {
+        this.cropSelected = cropSelected;
+    }
+
+    
+    
 }
