132611989c813a2ee5b06ab3cbc949e80d18fed1
Gabriel Costa Silva <gabriel_costasilva@yahoo.com.br>
2018-07-20 11:09:48 -0300

    Updated project

--- mip/src/main/java/br/edu/utfpr/cp/emater/mip/field/field/FieldRepository.java
+++ mip/src/main/java/br/edu/utfpr/cp/emater/mip/field/field/FieldRepository.java
@@ -1,9 +1,7 @@
 package br.edu.utfpr.cp.emater.mip.field.field;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource
 public interface FieldRepository extends JpaRepository<Field, Long>{
     
 }
