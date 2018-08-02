7807940850b668d00fd9309fc6847fecb675f756
Gabriel Costa Silva <gabriel_costasilva@yahoo.com.br>
2018-08-02 18:52:42 -0300

    - Created structure for MacroRegion CRUD.

---
+++ mip/src/main/java/br/edu/utfpr/cp/emater/mip/view/MacroRegionController.java
@@ -0,0 +1,15 @@
+package br.edu.utfpr.cp.emater.mip.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class MacroRegionController {
+    
+    @GetMapping ("/macroregion")
+    public String list() {
+        return "/field/macroregion/index";
+    }
+    
+}
