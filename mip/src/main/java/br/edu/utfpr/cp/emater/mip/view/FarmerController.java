2c7581b72981cbd72ac9897bef20c3fe92c3275f
Gabriel Costa Silva <gabriel_costasilva@yahoo.com.br>
2018-08-03 21:46:30 -0300

    - Created GUI CRUD for Farmer and Supervisor (Person).

---
+++ mip/src/main/java/br/edu/utfpr/cp/emater/mip/view/FarmerController.java
@@ -0,0 +1,107 @@
+package br.edu.utfpr.cp.emater.mip.view;
+
+import br.edu.utfpr.cp.emater.mip.domain.field.person.Farmer;
+import br.edu.utfpr.cp.emater.mip.domain.field.person.FarmerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+enum FarmerLabels {
+    PAGE_TITLE ("Gerenciamento de Produtores"),
+    ENTITY ("Produtor"),
+    ARTICLE ("o"),
+    URL_CREATE ("/farmer/create"),
+    URL_UPDATE ("/farmer/update"),
+    URL_DELETE ("/farmer/delete");
+    
+    private String value;
+    
+    FarmerLabels (String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
+
+enum FarmerPath {
+    SUCCESS_CREATE ("/farmer"),
+    SUCCESS_UPDATE ("/farmer"),
+    SUCCESS_DELETE ("/farmer"),
+    SUCCESS_READ ("/farmer"),
+    TEMPLATE_PATH ("/field/farmer/index");
+    
+    private String value;
+    
+    FarmerPath (String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
+
+
+@Controller
+@RequestMapping (value = "/farmer")
+public class FarmerController {
+    
+    private final FarmerRepository repository;
+
+    @Autowired
+    public FarmerController(FarmerRepository repository) {
+        this.repository = repository;
+    }
+    
+    @RequestMapping (value = "", method = RequestMethod.GET)
+    public String listAll(Model data) {
+        data.addAttribute("farmers", repository.findAll());
+        
+        data.addAttribute("pageTitle", FarmerLabels.PAGE_TITLE.getValue());
+        data.addAttribute("article", FarmerLabels.ARTICLE.getValue());
+        data.addAttribute("entity", FarmerLabels.ENTITY.getValue());
+        data.addAttribute("urlCreate", FarmerLabels.URL_CREATE.getValue());
+        data.addAttribute("urlUpdate", FarmerLabels.URL_UPDATE.getValue());
+        data.addAttribute("urlDelete", FarmerLabels.URL_DELETE.getValue());
+        
+        return FarmerPath.TEMPLATE_PATH.getValue();
+    }
+    
+    @RequestMapping (value = "/create", method = RequestMethod.GET)
+    public String create (@RequestParam String name) {
+        
+        repository.save(new Farmer(null, name));
+        return FarmerPath.SUCCESS_CREATE.getValue();
+    }
+    
+    @RequestMapping (value = "/update", method = RequestMethod.GET)
+    public String update (@RequestParam String name, @RequestParam int id) {
+        
+        Farmer mr = repository.findById(new Long (id)).get();
+        mr.setName(name);
+        
+        repository.saveAndFlush(mr);
+        
+        return FarmerPath.SUCCESS_UPDATE.getValue();
+    }
+
+    @RequestMapping (value = "/delete", method = RequestMethod.GET)
+    public String delete (@RequestParam int id) {
+        repository.deleteById(new Long(id));
+        
+        return FarmerPath.SUCCESS_DELETE.getValue();
+    }
+}
---
+++ mip/src/main/java/br/edu/utfpr/cp/emater/mip/view/SupervisorController.java
@@ -0,0 +1,107 @@
+package br.edu.utfpr.cp.emater.mip.view;
+
+import br.edu.utfpr.cp.emater.mip.domain.field.person.Supervisor;
+import br.edu.utfpr.cp.emater.mip.domain.field.person.SupervisorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+enum SupervisorLabels {
+    PAGE_TITLE ("Gerenciamento de Responsáveis Técnicos"),
+    ENTITY ("Responsável Técnico"),
+    ARTICLE ("o"),
+    URL_CREATE ("/supervisor/create"),
+    URL_UPDATE ("/supervisor/update"),
+    URL_DELETE ("/supervisor/delete");
+    
+    private String value;
+    
+    SupervisorLabels (String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
+
+enum SupervisorPath {
+    SUCCESS_CREATE ("/supervisor"),
+    SUCCESS_UPDATE ("/supervisor"),
+    SUCCESS_DELETE ("/supervisor"),
+    SUCCESS_READ ("/supervisor"),
+    TEMPLATE_PATH ("/field/supervisor/index");
+    
+    private String value;
+    
+    SupervisorPath (String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
+
+
+@Controller
+@RequestMapping (value = "/supervisor")
+public class SupervisorController {
+    
+    private final SupervisorRepository repository;
+
+    @Autowired
+    public SupervisorController(SupervisorRepository repository) {
+        this.repository = repository;
+    }
+    
+    @RequestMapping (value = "", method = RequestMethod.GET)
+    public String listAll(Model data) {
+        data.addAttribute("supervisors", repository.findAll());
+        
+        data.addAttribute("pageTitle", SupervisorLabels.PAGE_TITLE.getValue());
+        data.addAttribute("article", SupervisorLabels.ARTICLE.getValue());
+        data.addAttribute("entity", SupervisorLabels.ENTITY.getValue());
+        data.addAttribute("urlCreate", SupervisorLabels.URL_CREATE.getValue());
+        data.addAttribute("urlUpdate", SupervisorLabels.URL_UPDATE.getValue());
+        data.addAttribute("urlDelete", SupervisorLabels.URL_DELETE.getValue());
+        
+        return SupervisorPath.TEMPLATE_PATH.getValue();
+    }
+    
+    @RequestMapping (value = "/create", method = RequestMethod.GET)
+    public String create (@RequestParam String name) {
+        
+        repository.save(new Supervisor(null, name));
+        return SupervisorPath.SUCCESS_CREATE.getValue();
+    }
+    
+    @RequestMapping (value = "/update", method = RequestMethod.GET)
+    public String update (@RequestParam String name, @RequestParam int id) {
+        
+        Supervisor mr = repository.findById(new Long (id)).get();
+        mr.setName(name);
+        
+        repository.saveAndFlush(mr);
+        
+        return SupervisorPath.SUCCESS_UPDATE.getValue();
+    }
+
+    @RequestMapping (value = "/delete", method = RequestMethod.GET)
+    public String delete (@RequestParam int id) {
+        repository.deleteById(new Long(id));
+        
+        return SupervisorPath.SUCCESS_DELETE.getValue();
+    }
+}
