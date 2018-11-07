562d6fc77ceca16417a27189fc60a54d952c11f2
Gabriel Costa Silva <gabriel@Gabriels-iMac.local>
2018-11-07 18:07:59 -0200

    - Created properties file to define routes; - Updated MacroRegionController to use properties;

--- mip/src/main/java/br/edu/utfpr/cp/emater/mip/view/field/MacroRegionController.java
+++ mip/src/main/java/br/edu/utfpr/cp/emater/mip/view/field/MacroRegionController.java
@@ -1,58 +1,15 @@
 package br.edu.utfpr.cp.emater.mip.view.field;
 
-import br.edu.utfpr.cp.emater.mip.domain.field.macroregion.MacroRegion;
-import br.edu.utfpr.cp.emater.mip.domain.field.macroregion.MacroRegionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-enum MacroRegionLabels {
-    PAGE_TITLE ("Gerenciamento de Macrorregiões"),
-    ENTITY ("Macrorregião"),
-    ARTICLE ("a"),
-    URL_CREATE ("/macroregion/create"),
-    URL_UPDATE ("/macroregion/update"),
-    URL_DELETE ("/macroregion/delete");
-    
-    private String value;
-    
-    MacroRegionLabels (String value) {
-        this.value = value;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
-}
-
-enum MacroRegionPath {
-    SUCCESS_CREATE ("redirect:/macroregion"),
-    SUCCESS_UPDATE ("redirect:/macroregion"),
-    SUCCESS_DELETE ("redirect:/macroregion"),
-    SUCCESS_READ ("redirect:/macroregion"),
-    TEMPLATE_PATH ("/field/macroregion/index");
-    
-    private String value;
-    
-    MacroRegionPath (String value) {
-        this.value = value;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
-}
+import br.edu.utfpr.cp.emater.mip.domain.field.macroregion.MacroRegion;
+import br.edu.utfpr.cp.emater.mip.domain.field.macroregion.MacroRegionRepository;
 
 
 @Controller
@@ -61,23 +18,26 @@ public class MacroRegionController {
     
     private final MacroRegionRepository repository;
 
+    private final Environment environment;
+
     @Autowired
-    public MacroRegionController(MacroRegionRepository repository) {
+    public MacroRegionController(MacroRegionRepository repository, Environment environment) {
         this.repository = repository;
+        this.environment = environment;
     }
     
     @RequestMapping (value = "", method = RequestMethod.GET)
     public String listAll(Model data) {
         data.addAttribute("macroregions", repository.findAll());
         
-        data.addAttribute("pageTitle", MacroRegionLabels.PAGE_TITLE.getValue());
-        data.addAttribute("article", MacroRegionLabels.ARTICLE.getValue());
-        data.addAttribute("entity", MacroRegionLabels.ENTITY.getValue());
-        data.addAttribute("urlCreate", MacroRegionLabels.URL_CREATE.getValue());
-        data.addAttribute("urlUpdate", MacroRegionLabels.URL_UPDATE.getValue());
-        data.addAttribute("urlDelete", MacroRegionLabels.URL_DELETE.getValue());
+        data.addAttribute("pageTitle", environment.getProperty("app.view.title.page.field.macroregion"));
+        data.addAttribute("article", environment.getProperty("app.view.title.article.female"));
+        data.addAttribute("entity", environment.getProperty("app.view.title.entity.field.macroregion"));
+        data.addAttribute("urlCreate", environment.getProperty("app.view.route.create.field.macroregion"));
+        data.addAttribute("urlUpdate", environment.getProperty("app.view.route.update.field.macroregion"));
+        data.addAttribute("urlDelete", environment.getProperty("app.view.route.delete.field.macroregion"));
         
-        return MacroRegionPath.TEMPLATE_PATH.getValue();
+        return environment.getProperty("app.view.route.template.main.field.macroregion");
     }
     
     @RequestMapping (value = "/create", method = RequestMethod.POST)
@@ -88,7 +48,7 @@ public class MacroRegionController {
 
         repository.save(mr);
         
-        return MacroRegionPath.SUCCESS_CREATE.getValue();
+        return environment.getProperty("app.view.route.create.success.field.macroregion");
     }
     
     @RequestMapping (value = "/update", method = RequestMethod.POST)
@@ -99,13 +59,13 @@ public class MacroRegionController {
         
         repository.saveAndFlush(mr);
         
-        return MacroRegionPath.SUCCESS_UPDATE.getValue();
+        return environment.getProperty("app.view.route.update.success.field.macroregion");
     }
 
     @RequestMapping (value = "/delete", method = RequestMethod.POST)
     public String delete (@RequestParam int id) {
         repository.deleteById(new Long(id));
         
-        return MacroRegionPath.SUCCESS_DELETE.getValue();
+        return environment.getProperty("app.view.route.delete.success.field.macroregion");
     }
 }
