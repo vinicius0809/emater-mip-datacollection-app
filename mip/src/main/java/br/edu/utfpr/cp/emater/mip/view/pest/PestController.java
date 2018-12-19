6db2c684bce00133e69c925a868941f663c743b0
Gabriel Costa Silva <gabriel_costasilva@yahoo.com.br>
2018-12-19 15:56:22 -0200

    - Updated routes in Pest controller

--- mip/src/main/java/br/edu/utfpr/cp/emater/mip/view/pest/PestController.java
+++ mip/src/main/java/br/edu/utfpr/cp/emater/mip/view/pest/PestController.java
@@ -1,12 +1,11 @@
 package br.edu.utfpr.cp.emater.mip.view.pest;
 
-import br.edu.utfpr.cp.emater.mip.domain.field.macroregion.MacroRegion;
-import br.edu.utfpr.cp.emater.mip.domain.field.macroregion.MacroRegionRepository;
 import br.edu.utfpr.cp.emater.mip.domain.pest.Pest;
 import br.edu.utfpr.cp.emater.mip.domain.pest.PestRepository;
 import br.edu.utfpr.cp.emater.mip.domain.pest.PestSize;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,10 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 enum PestLabels {
     PAGE_TITLE ("Gerenciamento de Insetos Praga"),
     ENTITY ("Praga"),
-    ARTICLE ("a"),
-    URL_CREATE ("/pest/create"),
-    URL_UPDATE ("/pest/update"),
-    URL_DELETE ("/pest/delete");
+    ARTICLE ("a");
     
     private String value;
     
@@ -36,38 +32,18 @@ enum PestLabels {
     }
 }
 
-enum PestPath {
-    SUCCESS_CREATE ("redirect:/pest"),
-    SUCCESS_UPDATE ("redirect:/pest"),
-    SUCCESS_DELETE ("redirect:/pest"),
-    SUCCESS_READ ("redirect:/pest"),
-    TEMPLATE_PATH ("/pest/pest/index");
-    
-    private String value;
-    
-    PestPath (String value) {
-        this.value = value;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
-}
-
-
 @Controller
 @RequestMapping (value = "/pest")
 public class PestController {
     
     private final PestRepository repository;
 
+    private final Environment environment;
+
     @Autowired
-    public PestController(PestRepository repository) {
+    public PestController(PestRepository repository, Environment environment) {
         this.repository = repository;
+        this.environment = environment;
     }
     
     @RequestMapping (value = "", method = RequestMethod.GET)
@@ -78,11 +54,11 @@ public class PestController {
         data.addAttribute("pageTitle", PestLabels.PAGE_TITLE.getValue());
         data.addAttribute("article", PestLabels.ARTICLE.getValue());
         data.addAttribute("entity", PestLabels.ENTITY.getValue());
-        data.addAttribute("urlCreate", PestLabels.URL_CREATE.getValue());
-        data.addAttribute("urlUpdate", PestLabels.URL_UPDATE.getValue());
-        data.addAttribute("urlDelete", PestLabels.URL_DELETE.getValue());
+        data.addAttribute("urlCreate", this.environment.getProperty("app.view.route.create.mip.pest"));
+        data.addAttribute("urlUpdate", this.environment.getProperty("app.view.route.update.mip.pest"));
+        data.addAttribute("urlDelete", this.environment.getProperty("app.view.route.delete.mip.pest"));
         
-        return PestPath.TEMPLATE_PATH.getValue();
+        return this.environment.getProperty("app.view.route.template.main.mip.pest");
     }
     
     @RequestMapping (value = "/create", method = RequestMethod.POST)
@@ -90,7 +66,7 @@ public class PestController {
         
         repository.save(pest);
         
-        return PestPath.SUCCESS_CREATE.getValue();
+        return this.environment.getProperty("app.view.route.create.success.mip.pest");
     }
     
     @RequestMapping (value = "/update", method = RequestMethod.POST)
@@ -103,13 +79,13 @@ public class PestController {
         
         repository.saveAndFlush(originalPest);
         
-        return PestPath.SUCCESS_UPDATE.getValue();
+        return this.environment.getProperty("app.view.route.update.success.mip.pest");
     }
 
     @RequestMapping (value = "/delete", method = RequestMethod.POST)
     public String delete (@RequestParam int id) {
         repository.deleteById(new Long(id));
         
-        return PestPath.SUCCESS_DELETE.getValue();
+        return this.environment.getProperty("app.view.route.delete.success.mip.pest");
     }
 }
