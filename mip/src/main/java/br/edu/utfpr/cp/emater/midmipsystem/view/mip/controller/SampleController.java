7ab962570b0ef44873062bf41d6b2bd9059cda24
Gabriel Costa Silva <gabriel@Gabriels-iMac.local>
2019-03-23 15:50:16 -0300

    - This closes #39; - This closes #16.

--- mip/src/main/java/br/edu/utfpr/cp/emater/midmipsystem/view/mip/controller/SampleController.java
+++ mip/src/main/java/br/edu/utfpr/cp/emater/midmipsystem/view/mip/controller/SampleController.java
@@ -20,6 +20,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+// TO FIX !!!
+
 @Controller
 @RequestMapping (value = "/pest-survey/list-collected-samples")
 public class SampleController {
